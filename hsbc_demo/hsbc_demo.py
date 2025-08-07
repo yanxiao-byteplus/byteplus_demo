a7ae68073baec7d3538bae75a755938aa0163884
yan.xiao <yan.xiao@bytedance.com>
2025-08-07 14:14:27 +0800

    hsbc_demo

--- hsbc_demo/hsbc_demo.py
+++ hsbc_demo/hsbc_demo.py
@@ -1,11 +1,15 @@
 #  -*- coding: utf-8 -*-
-import os,sys
+import os
+import sys
+import json
 
 sys.path.insert(0, "/Users/bytedance/byteplus/byteplus-sdk-python/")
+
 from byteplus_sdk.cdn.service import CDNService
 
-from rule_engine.Rule import Condition, Action, Rule
-from rule_engine.Const import Const
+from cdn_rule_engine_sdk.rule_engine.Rule import Condition, Action, Rule
+from cdn_rule_engine_sdk.rule_engine.Const import Const
+from hsbc_demo_json import json_delivery_policy
 
 class HSBCDemo:
     def __init__(self):
@@ -14,48 +18,9 @@ class HSBCDemo:
         sk = os.environ.get('HSBC_DEMO_SECRETKEY', '')
         self._svc.set_ak(ak)
         self._svc.set_sk(sk)
+        self._message = 'Byteplus SDK HSBC Demo'
 
-
-    # def create_delivery_policy(self, policy_name: str) -> str:
-                
-    #     body = {
-    #         "Title": policy_name,
-    #         "Message": "SDK Demo",
-    #         "Project": "default",
-    #         "OriginProtocol": "http",
-    #         "Origin": [
-    #             {
-    #                 "OriginAction": {
-    #                     "OriginLines": [
-    #                         {
-    #                             "OriginType": "primary",
-    #                             "InstanceType": "domain",
-    #                             "Address": "img.migrate.lcyice.top",
-    #                             "Weight": "1",
-    #                             "HttpPort": "80",
-    #                             "HttpsPort": "443",
-    #                             "OriginHost": "img.migrate.lcyice.top",
-       
-    #                         }
-    #                     ]
-    #                 }
-    #             }
-    #         ]
-    #     }
-
-    #     resp = self._svc.create_service_template(body)
-        
-    #     try:
-    #         template_id = resp['Result']['TemplateId']
-    #         if template_id:
-    #             print(f"""
-    #                 delivery policy created as a success {policy_name=} -> {template_id=}
-    #             """)
-    #             return template_id
-    #     except KeyError:
-    #         print(resp)
-        
-    
+     
     def list_cdn_domains(self) -> None:
         body = {}
         data = self._svc.list_cdn_domains(body)
@@ -63,36 +28,62 @@ class HSBCDemo:
             print(f"""
                   {seq} domain: {domain['Domain']}  cname: {domain['Cname']}  waf: {domain['Waf']} 
             """)
-    
-    # def describe_delivery_policy(self, template_id: str) -> None:
-        
-    #     if template_id:
             
-    #         body = {
-    #             "TemplateId": template_id
-    #         }
-
-    #         data = self._svc.describe_service_template(body)
-    #         try:
-    #             policy = data["Result"]
-    #             template_id = policy['TemplateId']
-    #             title = policy['Title']
+    def create_delivery_policy(self, title: str) -> str:
                 
-    #             domains = []
-    #             if policy['BoundDomains']:
-    #                 domains = [x['Domain'] for x in policy['BoundDomains']]
+        json_policy_metadata = f"""
+            "Title": "{title}",
+            "Message": "{self._message}",
+            "Project": "default",
+        """
+        
+        json_full_str = "{" + json_policy_metadata + json_delivery_policy + "}"
+
+        try:
+            body = json.loads(json_full_str.strip().strip("\n"))
+            resp = self._svc.create_service_template(body)
+            template_id = resp['Result']['TemplateId']
+            if template_id:
+                print(f"""
+                    delivery policy created as a success {title=} -> {template_id=}
+                """)
+                return template_id
+        except KeyError:
+            print(resp)
+        except json.JSONDecodeError as e:
+            print(f"json decoding exception {e=}")
+
+    
+    def describe_delivery_policy(self, template_id: str) -> None:
+        
+        if template_id:
+            
+            body = {
+                "TemplateId": template_id
+            }
+
+            data = self._svc.describe_service_template(body)
+            try:
+                policy = data["Result"]
+                template_id = policy['TemplateId']
+                title = policy['Title']
+                
+                domains = []
+                if policy['BoundDomains']:
+                    domains = [x['Domain'] for x in policy['BoundDomains']]
                     
-    #             print(f"""
-    #                 {template_id=}  {title=} -> {domains=}
-    #             """)
-    #         except (KeyError):
-    #             print(data)
+                print(f"""
+                    {template_id=}  {title=} -> {domains=}
+                """)
+                
+            except (KeyError):
+                print(data)
 
     def create_cipher_policy(self, title: str) -> None:
         
         body = {
             "Title": title,
-            "Message": "hsbc demo",
+            "Message": self._message,
             "Project": "default",
             "Quic": {
                 "Switch": False
@@ -119,8 +110,8 @@ class HSBCDemo:
             },
         }
         
-        data = self._svc.create_cipher_template(body)
         try:
+            data = self._svc.create_cipher_template(body)
             policy = data["Result"]
             template_id = policy['TemplateId']
               
@@ -164,6 +155,7 @@ class HSBCDemo:
             
         except Exception as e:
             print(f"{e=}")
+    
             
     def update_cipher_template(self, template_id: str) -> None:
         
@@ -184,7 +176,8 @@ class HSBCDemo:
     def release_policy(self, template_id: str) -> None:
         
         body = {
-            "TemplateId": template_id        }
+            "TemplateId": template_id       
+        }
 
         try:
             resp = self._svc.release_template(body)
@@ -196,10 +189,6 @@ class HSBCDemo:
             print(f"{e=}")
             
             
-        body = {
-        "TemplateId": "tpl-example"
-    }
-
 
     def delete_policy(self, template_id: str) -> None:
         
@@ -217,28 +206,105 @@ class HSBCDemo:
             print(f"{e=}")
             
     
-# tpl-ce9g3ro
+    def create_rule_engine(self, title: str) -> str:
+        
+        rule = Rule()
+        rule.desc = self._message
+        rule.if_block.condition = Condition({
+            "IsGroup": True,
+            "Connective": Const.ConnectiveAnd,
+            "ConditionGroups": [
+                {
+                    "IsGroup": True,
+                    "Connective": Const.ConnectiveAnd,
+                    "ConditionGroups": [
+                        {
+                            "IsGroup": False,
+                            "Connective": Const.ConnectiveAnd,
+                            "Condition": {
+                                "Object": Const.ConditionHTTPPath,
+                                "Operator":Const.OperatorPrefixMatch,
+                                "IgnoreCase":True,
+                                "Value":["/stripheader/"]
+                            }
+                        }
+
+                    ]
+                }
+            ]
+        })
+        rule.if_block.actions.append(Action({
+            "Action": Const.ActionResponseHeader,
+            "Groups":[
+                {
+                    "Dimension": Const.ActionResponseHeader,
+                    "GroupParameters":[
+                        {
+                            "Parameters":[
+                                {
+                                    "Name":"action","Values":[Const.Delete]
+                                },
+                                {
+                                    "Name":"header_name","Values":["server"]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }))
+
+      
+        try:
+            data = self._svc.create_rule_engine_template({
+                "Project": "default",
+                "Title": title,
+                "Message": self._message + ": delete response header server",
+                "Rule": rule.encode_to_string()
+            })
+               
+            policy = data["Result"]
+            template_id = policy['TemplateId']
+              
+            print(f"""
+                {template_id=} created for rule engine
+            """)
+            
+            return template_id
+        
+        except (KeyError) as e:
+            print(f"{e=}")
+            print(data)
+    
 
 if __name__ == '__main__':
     
-    demo = HSBCDemo()
-    # demo.list_cdn_domains()
-    # template_id = demo.create_delivery_policy(policy_name = "hsbc-sdk-api-demo")
-    # if template_id:
-    #     demo.describe_delivery_policy(template_id =  template_id)
-    
-    # template_ciphers = 'tpl-ceqmklq'
-    # if template_ciphers:
-    #     demo.describe_cipher_policy(template_id =  template_ciphers)、
+    byteplus_cdn_sdk = HSBCDemo()
+    byteplus_cdn_sdk.list_cdn_domains()
 
     title_tpl_cipher = 'tpl_hsbc_sdk_cipher'
-    # id_tpl_cipher = demo.create_cipher_policy(title =  title_tpl_cipher)
+    id_tpl_cipher = byteplus_cdn_sdk.create_cipher_policy(title =  title_tpl_cipher)
+    if id_tpl_cipher:
+        byteplus_cdn_sdk.release_policy(template_id = id_tpl_cipher)
+        # byteplus_cdn_sdk.update_cipher_template(template_id = id_tpl_cipher)
+        byteplus_cdn_sdk.describe_cipher_policy(template_id = id_tpl_cipher)
+        # byteplus_cdn_sdk.delete_policy(template_id = id_tpl_cipher)
     
-    # if id_tpl_cipher:
-    #     demo.release_policy(template_id = id_tpl_cipher)
-    #     demo.update_cipher_template(template_id = id_tpl_cipher)
-    #     demo.describe_cipher_policy(template_id = id_tpl_cipher)
-    #     demo.delete_policy(template_id = id_tpl_cipher)
+    title_tpl_rule_engine = 'tpl_hsbc_sdk_rule_engine'
+    id_tpl_rule_engine = byteplus_cdn_sdk.create_rule_engine(title = title_tpl_rule_engine)
+    if id_tpl_rule_engine:
+        byteplus_cdn_sdk.release_policy(template_id = id_tpl_rule_engine)
+        byteplus_cdn_sdk.describe_rule_engine_policy(template_id = id_tpl_rule_engine)
+        # byteplus_cdn_sdk.delete_policy(template_id = id_tpl_cipher)
+    
+    title_tpl_delivery_policy = 'tpl_hsbc_sdk_delivery_policy'    
+    id_tpl_delivery_policy = byteplus_cdn_sdk.create_delivery_policy(title = title_tpl_delivery_policy)
+    if id_tpl_delivery_policy:
+        byteplus_cdn_sdk.release_policy(template_id = id_tpl_delivery_policy)
+        byteplus_cdn_sdk.describe_delivery_policy(template_id = id_tpl_delivery_policy)
+        # byteplus_cdn_sdk.delete_policy(template_id = id_tpl_cipher)
+
+
 
 
 
---
+++ hsbc_demo/hsbc_demo_json.py
@@ -0,0 +1,294 @@
+json_delivery_policy = """
+  "AreaAccessRule": {
+    "Area": ["HKG", "MYS", "THA", "IDN", "USA", "RUS"],
+    "RuleType": "allow",
+    "Switch": true
+  },
+
+  "BrowserCache": [],
+  "Cache": [
+    {
+      "CacheAction": {
+        "Action": "cache",
+        "DefaultPolicy": "force_cache",
+        "IgnoreCase": false,
+        "Ttl": 1209600
+      },
+      "Condition": {
+        "ConditionRule": [
+          {
+            "Name": "",
+            "Object": "filetype",
+            "Operator": "match",
+            "Type": "url",
+            "Value": "html;htm;7z;avi;avif;apk;bin;bmp;bz2;class;css;csv;doc;docx;dmg;ejs;eot;eps;exe;flac;gif;gz;ico;iso;jar;jpg;jpeg;js;mid;midi;mkv;mp3;mp4;ogg;otf;pdf;pict;pls;png;ppt;pptx;ps;rar;svg;svgz;swf;tar;tif;tiff;ttf;webm;webp;woff;woff2;xls;xlsx;zip;zst"
+          }
+        ],
+        "Connective": "OR"
+      }
+    },
+    {
+      "CacheAction": {
+        "Action": "cache",
+        "DefaultPolicy": "origin_first_Replenish",
+        "IgnoreCase": false,
+        "Ttl": 0
+      },
+      "Condition": {
+        "ConditionRule": [
+          {
+            "Name": "",
+            "Object": "path",
+            "Operator": "match",
+            "Type": "url",
+            "Value": "/"
+          }
+        ],
+        "Connective": "OR"
+      }
+    }
+  ],
+  "CacheKey": [
+    {
+      "CacheKeyAction": {
+        "CacheKeyComponents": [
+          {
+            "Action": "include",
+            "IgnoreCase": true,
+            "Object": "queryString",
+            "Subobject": "*"
+          }
+        ]
+      },
+      "Condition": {
+        "ConditionRule": [
+          {
+            "Name": "",
+            "Object": "directory",
+            "Operator": "match",
+            "Type": "url",
+            "Value": "/"
+          }
+        ],
+        "Connective": "OR"
+      }
+    }
+  ],
+  "Compression": {
+    "CompressionRules": [
+      {
+        "CompressionAction": {
+          "CompressionFormat": "all",
+          "CompressionTarget": "*",
+          "CompressionType": ["gzip", "br"],
+          "MaxFileSizeKB": null,
+          "MinFileSizeKB": 1
+        },
+        "Condition": {
+          "ConditionRule": [
+            {
+              "Name": "",
+              "Object": "directory",
+              "Operator": "match",
+              "Type": "url",
+              "Value": "/hugefile/"
+            }
+          ],
+          "Connective": "OR"
+        }
+      }
+    ],
+    "Switch": true
+  },
+  "ConditionalOrigin": {
+    "OriginRules": [],
+    "Switch": false
+  },
+  "CreateTime": 1753410831,
+  "CustomErrorPage": {
+    "ErrorPageRule": [
+      {
+        "ErrorPageAction": {
+          "Action": "redirect",
+          "RedirectCode": "302",
+          "RedirectUrl": "https://hsbc-demo-101.migrate.lcyice.top/error_page.php?error_code=401&error_message=Oops...%20Something%20goes%20wrong...",
+          "StatusCode": "401"
+        }
+      },
+      {
+        "ErrorPageAction": {
+          "Action": "redirect",
+          "RedirectCode": "302",
+          "RedirectUrl": "https://hsbc-demo-101.migrate.lcyice.top/error_page.php?error_code=402&error_message=Oops...%20Something%20goes%20wrong...",
+          "StatusCode": "402"
+        }
+      },
+      {
+        "ErrorPageAction": {
+          "Action": "redirect",
+          "RedirectCode": "302",
+          "RedirectUrl": "https://hsbc-demo-101.migrate.lcyice.top/error_page.php?error_code=403&error_message=Oops...%20Something%20goes%20wrong...",
+          "StatusCode": "403"
+        }
+      },
+      {
+        "ErrorPageAction": {
+          "Action": "redirect",
+          "RedirectCode": "302",
+          "RedirectUrl": "https://hsbc-demo-101.migrate.lcyice.top/error_page.php?error_code=404&error_message=Oops...%20Something%20goes%20wrong...",
+          "StatusCode": "404"
+        }
+      },
+      {
+        "ErrorPageAction": {
+          "Action": "redirect",
+          "RedirectCode": "302",
+          "RedirectUrl": "https://hsbc-demo-101.migrate.lcyice.top/error_page.php?error_code=500&error_message=Oops...%20Something%20goes%20wrong...",
+          "StatusCode": "5xx"
+        }
+      }
+    ],
+    "Switch": true
+  },
+  "Exception": false,
+  "FollowRedirect": false,
+  "IPv6": {
+    "Switch": true
+  },
+  "Message": "",
+  "MethodDeniedRule": {
+    "Methods": "POST,DELETE,PUT,PATCH,CONNECT,OPTIONS",
+    "Switch": true
+  },
+  "NegativeCache": null,
+  "Origin": [
+    {
+      "Condition": null,
+      "OriginAction": {
+        "OriginLines": [
+          {
+            "Address": "34.124.182.199",
+            "BucketName": null,
+            "HttpPort": "80",
+            "HttpsPort": "443",
+            "InstanceType": "ip",
+            "OriginHost": "",
+            "OriginType": "primary",
+            "PrivateBucketAccess": false,
+            "PrivateBucketAuth": null,
+            "Region": "",
+            "Weight": "1"
+          }
+        ]
+      }
+    }
+  ],
+  "OriginAccessRule": {
+    "AllowEmpty": true,
+    "IgnoreCase": null,
+    "Origins": null,
+    "RuleType": null,
+    "Switch": null
+  },
+  "OriginArg": [
+    {
+      "Condition": {
+        "ConditionRule": [
+          {
+            "Name": "",
+            "Object": "directory",
+            "Operator": "match",
+            "Type": "url",
+            "Value": "/"
+          }
+        ],
+        "Connective": "OR"
+      },
+      "OriginArgAction": {
+        "OriginArgComponents": [
+          {
+            "Action": "include",
+            "Object": "queryString",
+            "Subobject": "*"
+          }
+        ]
+      }
+    }
+  ],
+  "OriginCertCheck": {
+    "CertInfoList": null,
+    "Switch": false
+  },
+  "OriginHost": "",
+  "OriginIPv6": "followclient",
+  "OriginProtocol": "http",
+  "OriginRange": false,
+  "OriginRetry": null,
+  "Project": "default",
+  "RemoteAuth": {
+    "RemoteAuthRules": null,
+    "Switch": false
+  },
+  "RequestHeader": [
+    {
+      "Condition": null,
+      "RequestHeaderAction": {
+        "RequestHeaderInstances": [
+          {
+            "Action": "set",
+            "Key": "x-byteplus-demo-client-ip",
+            "Value": "client_ip",
+            "ValueType": "variable"
+          },
+          {
+            "Action": "set",
+            "Key": "x-cdn-origin-server",
+            "Value": "origin_srv_addr",
+            "ValueType": "variable"
+          }
+        ]
+      }
+    }
+  ],
+  "ResponseHeader": [
+    {
+      "Condition": null,
+      "ResponseHeaderAction": {
+        "ResponseHeaderInstances": [
+          {
+            "AccessOriginControl": false,
+            "Action": "set",
+            "Key": "Pragma",
+            "Value": "request_uri",
+            "ValueType": "variable"
+          }
+        ]
+      }
+    }
+  ],
+  "SignedUrlAuth": {
+    "AuthRulesF": null,
+    "SignedUrlAuthRules": [
+      {
+        "Condition": {
+          "ConditionRule": null,
+          "Connective": null
+        },
+        "SignedUrlAuthAction": null
+      }
+    ],
+    "Switch": null
+  },
+  "Type": "service",
+  "UaAccessRule": {
+    "AllowEmpty": true,
+    "IgnoreCase": null,
+    "RuleType": null,
+    "Switch": null,
+    "UserAgent": null
+  },
+  "UrlNormalize": {
+    "NormalizeObject": ["back_slashes", "successive_slashes", "dot_segments"],
+    "Switch": true
+  }
+"""
